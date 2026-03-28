493ae52418a70f62a45a5faca9dd317063e8f0a8
Yasunobu_Watanabe <pockorcky@gmail.com>
2026-03-28 15:39:09 +0000

    ✨ feat(logging): ロギング設定を追加

---
+++ api/app/setup/logging.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
+
+from pythonjsonlogger.json import JsonFormatter
+
+from app.config import settings
+
+
+def configure_logging() -> None:
+    formatter = JsonFormatter(
+        "%(asctime)s - %(levelname)s - %(message)s - %(pathname)s - %(lineno)d - %(process)d"
+    )
+
+    if settings.stage == "dev":
+        logging.basicConfig(level=logging.DEBUG)
+
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setLevel(logging.DEBUG)
+        sh.setFormatter(formatter)
+        logging.getLogger().addHandler(sh)
+
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+        fh = TimedRotatingFileHandler("api.log", when="midnight", interval=1, backupCount=7)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logging.getLogger().addHandler(fh)
